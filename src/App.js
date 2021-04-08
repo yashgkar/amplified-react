ba8577d62a32b5bd47105f6e61fd73e22963c77d
Prafful Patil <yashgarudkar@gmail.com>
2021-04-09 01:10:24 +0530

    Base Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+	Container,
+	Paper,
+	TextField,
+	Box,
+	Button,
+	List,
+	Collapse,
+	ListItem,
+	ListItemText,
+  Divider,
+} from '@material-ui/core'
+import { MdExpandLess, MdExpandMore } from 'react-icons/md'
+import { makeStyles } from '@material-ui/core/styles'
+import { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+	inputFields: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	formContainer: {
+		width: '100%',
+		padding: '1rem',
+		marginTop: '2rem',
+	},
+	inputField: {
+		width: '75%',
+	},
+	listStyling: {
+		width: '100%',
+		maxWidth: 600,
+		backgroundColor: theme.palette.background.paper,
+	},
+	nested: {
+		paddingLeft: theme.spacing(4),
+	},
+}))
+
+const App = () => {
+	const classes = useStyles()
+	const [tasks, setTasks] = useState([])
+	const [selectedIndex, setSelectedIndex] = useState('')
+
+	const handleClick = (index) => {
+		if (selectedIndex === index) {
+			setSelectedIndex('')
+		} else {
+			setSelectedIndex(index)
+		}
+	}
+
+	useEffect(() => {
+		setTasks([
+			{ id: '1', task: 'Hello world', description: 'yash here' },
+			{ id: '2', task: 'Hello world', description: 'yash here' },
+		])
+	}, [])
+
+	return (
+		<div>
+			<Container maxWidth='sm'>
+				<Paper elevation={2} className={classes.formContainer}>
+					<Box className={classes.inputFields}>
+						<TextField
+							className={classes.inputField}
+							label='Task name'
+							variant='outlined'
+						/>
+						<Button type='submit' variant='contained' color='primary'>
+							Add Task
+						</Button>
+					</Box>
+				</Paper>
+				<Paper elevation={3} className={classes.formContainer}>
+					<List
+						component='nav'
+						aria-labelledby='nested-list-subheader'
+						className={classes.listStyling}
+					>
+						{(tasks || []).map((item, index) => (
+							<div key={index}>
+								<ListItem button onClick={() => handleClick(index)}>
+									<ListItemText primary={item.task} />
+									{index === selectedIndex ? (
+										<MdExpandLess />
+									) : (
+										<MdExpandMore />
+									)}
+								</ListItem>
+								<Collapse
+									in={index === selectedIndex}
+									timeout='auto'
+									unmountOnExit
+								>
+									<List component='div' disablePadding>
+										<ListItem button className={classes.nested}>
+											<ListItemText primary={item.description} />
+										</ListItem>
+									</List>
+								</Collapse>
+                <Divider />
+							</div>
+						))}
+					</List>
+				</Paper>
+			</Container>
+		</div>
+	)
 }
 
-export default App;
+export default App
