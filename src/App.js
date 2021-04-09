c670e157573141eea5348e3b924922e5a07d5bc8
Prafful Patil <yashgarudkar@gmail.com>
2021-04-09 19:25:31 +0530

    feat: Final code

--- src/App.js
+++ src/App.js
@@ -8,18 +8,23 @@ import {
 	Collapse,
 	ListItem,
 	ListItemText,
-  Divider,
+	Divider,
+	ListItemSecondaryAction,
+	IconButton
 } from '@material-ui/core'
-import { MdExpandLess, MdExpandMore } from 'react-icons/md'
+import { MdDelete, MdExpandLess, MdExpandMore } from 'react-icons/md'
 import { makeStyles } from '@material-ui/core/styles'
 import { useEffect, useState } from 'react'
 
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import { createTodo, deleteTodo } from './graphql/mutations'
+import { listTodos } from './graphql/queries'
+import awsExports from './aws-exports'
+import { withAuthenticator } from '@aws-amplify/ui-react'
+
+Amplify.configure(awsExports)
+
 const useStyles = makeStyles((theme) => ({
-	inputFields: {
-		display: 'flex',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-	},
 	formContainer: {
 		width: '100%',
 		padding: '1rem',
@@ -27,6 +32,7 @@ const useStyles = makeStyles((theme) => ({
 	},
 	inputField: {
 		width: '75%',
+		marginTop: '2rem',
 	},
 	listStyling: {
 		width: '100%',
@@ -38,9 +44,12 @@ const useStyles = makeStyles((theme) => ({
 	},
 }))
 
+const initialState = { name: '', description: '' }
+
 const App = () => {
 	const classes = useStyles()
 	const [tasks, setTasks] = useState([])
+	const [formState, setFormState] = useState(initialState)
 	const [selectedIndex, setSelectedIndex] = useState('')
 
 	const handleClick = (index) => {
@@ -52,25 +61,70 @@ const App = () => {
 	}
 
 	useEffect(() => {
-		setTasks([
-			{ id: '1', task: 'Hello world', description: 'yash here' },
-			{ id: '2', task: 'Hello world', description: 'yash here' },
-		])
+		fetchTodos()
 	}, [])
+
+	const handleChange = (event) => {
+		setFormState({ ...formState, [event.target.name]: event.target.value })
+	}
+
+	const fetchTodos = async () => {
+		const todo = await API.graphql(graphqlOperation(listTodos))
+		setTasks(todo.data.listTodos.items)
+	}
+
+	const createTask = async (e) => {
+		e.preventDefault()
+		try {
+			if (formState.description && formState.name) {
+				const todo = {
+					name: formState.name,
+					description: formState.description,
+				}
+				await API.graphql(graphqlOperation(createTodo, { input: todo }))
+				setFormState(initialState)
+				fetchTodos()
+			}
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	const deleteTodoById = async (id) => {
+		try {
+			await API.graphql(graphqlOperation(deleteTodo, { input: { id } }))
+			fetchTodos()
+		} catch (err) {
+			console.log(err)
+		}
+	}
 
 	return (
 		<div>
 			<Container maxWidth='sm'>
 				<Paper elevation={2} className={classes.formContainer}>
-					<Box className={classes.inputFields}>
-						<TextField
-							className={classes.inputField}
-							label='Task name'
-							variant='outlined'
-						/>
-						<Button type='submit' variant='contained' color='primary'>
-							Add Task
-						</Button>
+					<Box>
+						<form onSubmit={createTask}>
+							<TextField
+								className={classes.inputField}
+								label='Task name'
+								variant='outlined'
+								value={formState.name}
+								onChange={(e) => handleChange(e)}
+								name='name'
+							/>
+							<TextField
+								className={classes.inputField}
+								label='Description'
+								variant='outlined'
+								value={formState.description}
+								onChange={(e) => handleChange(e)}
+								name='description'
+							/>
+							<Button type='submit' variant='contained' color='primary'>
+								Add Task
+							</Button>
+						</form>
 					</Box>
 				</Paper>
 				<Paper elevation={3} className={classes.formContainer}>
@@ -82,7 +136,12 @@ const App = () => {
 						{(tasks || []).map((item, index) => (
 							<div key={index}>
 								<ListItem button onClick={() => handleClick(index)}>
-									<ListItemText primary={item.task} />
+									<ListItemText primary={item.name} />
+									<ListItemSecondaryAction>
+										<IconButton onClick={() => deleteTodoById(item.id)}>
+											<MdDelete />
+										</IconButton>
+									</ListItemSecondaryAction>
 									{index === selectedIndex ? (
 										<MdExpandLess />
 									) : (
@@ -100,7 +159,7 @@ const App = () => {
 										</ListItem>
 									</List>
 								</Collapse>
-                <Divider />
+								<Divider />
 							</div>
 						))}
 					</List>
@@ -110,4 +169,4 @@ const App = () => {
 	)
 }
 
-export default App
+export default withAuthenticator(App)
